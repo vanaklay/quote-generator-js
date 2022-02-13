1a0d2bb0057333ce88e0be446c8941a5389f45cd
vanaklay <ven2kaze@gmail.com>
2022-02-13 18:09:19 +0100

    add loader

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const french = document.getElementById('french');
 const khmer = document.getElementById('khmer');
 const facebookBtn = document.getElementById('facebook');
 const newBtn = document.getElementById('new');
+const loader = document.getElementById('loader');
 
 const getQuotes = async () => {
   const apiUrl = 'https://type.fit/api/quotes';
@@ -15,24 +16,42 @@ const getQuotes = async () => {
   }
 };
 
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
+
 const getRandomInt = (max) => Math.floor(Math.random() * max);
 
 let index;
 const getQuote = () => {
+  loading();
   index = getRandomInt(data.length);
   french.textContent = data[index].fr;
   khmer.textContent = data[index].kh;
+  complete();
 };
+
+const shareOnFacebook = () => {
+  // TODO: create a account FB developer
+  // FB.ui(
+  //   {
+  //     display: 'popup',
+  //     method: 'share',
+  //     quote: `${data[index].fr} - ${data[index].kh}`,
+  //   },
+  //   function (response) {}
+  // );
+  alert('OOOUUUUPS, problèmes !');
+};
+
+newBtn.addEventListener('click', getQuote);
+
+facebookBtn.addEventListener('click', shareOnFacebook);
 
 getQuote();
-
-facebookBtn.onclick = function () {
-  FB.ui(
-    {
-      display: 'popup',
-      method: 'share',
-      quote: `${data[index].fr} - ${data[index].kh}`,
-    },
-    function (response) {}
-  );
-};
